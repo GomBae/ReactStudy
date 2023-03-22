e3a9f8c61be8588d998dc9d15ddb19b31f4a1b09
GomBae <myg177@naver.com>
2023-03-22 18:48:26 +0900

    feat : Hooks를 이용한 main페이지 데이터 출력

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Component} from "react";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import Header from './components/main/Header';
+import Home from "./components/main/Home";
+import location from "./components/seoul/Location";
+import nature from "./components/seoul/Nature";
+import shop from "./components/seoul/Shop";
+import Location from "./components/seoul/Location";
+import Nature from "./components/seoul/Nature";
+import Shop from "./components/seoul/Shop";
+import FoodList from "./components/food/FoodList";
+import FoodDetail from "./components/food/FoodDetail";
+class App extends Component{
+    render() {
+        return(
+            <Router>
+                <Header/>
+                <div className={"container"}>
+                    <Routes>
+                        <Route exact path={"/"} element={<Home/>}/>
+                        <Route path={"/seoul/location"} element={<Location/>}/>
+                        <Route path={"/seoul/nature"} element={<Nature/>}/>
+                        <Route path={"/seoul/shop"} element={<Shop/>}/>
+                        <Route path={"/food/food_list/:cno"} element={<FoodList/>}/>
+                        <Route path={"/food/food_detail/:fno"} element={<FoodDetail/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
